4d4abfa3beba936518987fcd40842de9c8084ce5
Vlad <vladpoienariU@gmail.com>
2025-09-05 15:34:48 +0300

    get IP dest, src get Mac dest, src from live packets

--- packets-sniffing/identify_attacks.py
+++ packets-sniffing/identify_attacks.py
@@ -1,7 +1,24 @@
 from sniff_network import capture_live_packets
 
 def listen_arp_spoofing(packet):
-    print(packet)
+    ip_dest, ip_src = "", ""
+    mac_dest, mac_src = "", ""
+   
+
+    if "IP" in packet:
+        ip_dest = packet.ip.dst
+        ip_src = packet.ip.src
+    if "ETH" in packet:
+        mac_dest = packet.eth.dst
+        mac_src = packet.eth.src
+
+    print(
+        ip_src,
+        ip_dest,
+        mac_dest,
+        mac_src
+    )
+    
 
 def listen_arp_spoofing_call():
     capture_live_packets(listen_arp_spoofing)
