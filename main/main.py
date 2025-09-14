6972d9e9f1f774cdaa671eb32f8db8eaff0033b8
Your Name <your.email@example.com>
2025-09-14 18:40:45 +0300

    code cleanup split de code for menu UI optimization removed find main interfaces function

--- main/main.py
+++ main/main.py
@@ -1,21 +1,12 @@
 from identify_attacks import listen_arp_spoofing_call
-from helpers import  find_main_interfaces, get_network_ip, binary_to_decimanal_address
+from helpers import  find_interfaces, get_network_ip, binary_to_decimanal_address
 
-print(
-    binary_to_decimanal_address("11000000101010001010010100000000")
-)
-
-while True:
-    interfaces = find_main_interfaces()
-
-    device_IP = ""
-    netmask = ""
-    current_interface_name = ''
-
+def display_available_interfaces(interfaces):
     print("Available interfaces")
+
     for index in range(len(interfaces)):
         interf = interfaces[index]
-
+        
         if index == 0:
             device_IP = interf[1]
             netmask = interf[2]
@@ -23,8 +14,24 @@ while True:
 
         print(f"{'=> ' if index == 0 else ""} {interf[0]}: {interf[1]} {interf[2]}")
 
-    network_ip = get_network_ip(device_IP, netmask)
 
+while True:
+    interfaces = find_interfaces()
+
+    device_IP = interfaces[0][1]
+    netmask = interfaces[0][2]
+    current_interface_name = interfaces[0][0]
+
+    # Display available interfaces
+    if len(interfaces) == 0:
+        print("No interfaces found! ;(\nPlease try again later..")
+        
+        break 
+    else:
+        display_available_interfaces(interfaces)
+        network_ip = get_network_ip(device_IP, netmask)
+
+    # --- User UI Menu
     print("\nWhat u wanna do?\n[1] -> Listen for ARP Spoofing")
     option = input("Option: ")
 
